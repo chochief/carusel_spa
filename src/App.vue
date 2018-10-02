464a9cda0f62d2754cb7cd9d144703eadc62ad40
cho.chief <k.n.svetlakov@yandex.ru>
2018-10-02 12:13:01 +0400

    Added normilize.css and created sticky header

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <Hi />
+    <sticky-header />
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
@@ -10,21 +10,20 @@
 </template>
 
 <script>
-import Hi from './components/Hi'
+import StickyHeader from './components/StickyHeader'
 export default {
   components: {
-    Hi
+    StickyHeader
   }
 }
 </script>
-
 
 <style lang="scss">
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  // text-align: center;
   color: #2c3e50;
 }
 #nav {
--- src/components/StickyHeader.vue
+++ src/components/StickyHeader.vue
@@ -1,12 +1,90 @@
 <template lang="pug">
-  
+  .wrapper(id="wrapper")
+    header.header(id="header")
+      .container
+        .logo(id="logo") Carusel SPA
+        nav.navbar
+          a(href="#").nav__item Home
+          a(href="#").nav__item About US
+          a(href="#").nav__item Contact Us
 </template>
 
 <script>
-export default {}
+export default {
+  methods: {
+    handleScroll () {
+      let header = document.getElementById('header')
+      if (window.scrollY > 1) {
+        header.classList.add('sticky')
+      } else {
+        header.classList.remove('sticky')
+      }
+    }
+  },
+  created () {
+    window.addEventListener('scroll', this.handleScroll)
+  },
+  destroyed () {
+    window.removeEventListener('scroll', this.handleScroll)
+  }
+}
 </script>
 
-<style lang="sass" scoped>
+<style lang="scss" scoped>
+.wrapper {
+    width: 100%;
+    height: auto;
+    padding: 0;
+    margin: 0;
+    overflow: hidden;
+    min-height: 2000px;
+  }
 
+.header {
+  width: 100%;
+  height: 80px;
+  background-color: #446cb3;
+  color: #ffffff;
+  line-height: 80px;
+  position: fixed;
+  top: 0;
+  transition: all 0.5s ease;
+  &.sticky {
+    background-color: rgba(211,84,0,.9);
+    height: 50px;
+    line-height: 50px;
+    & .logo {
+      font-size: 22px;
+    }
+    & .nav__item {
+      font-size: 14px;
+    }
+  }
+}
+
+.container {
+  width: 100%;
+  max-width: 1024px;
+  margin: 0 auto;
+}
+
+.logo {
+  font-family: oswald;
+  font-size: 40px;
+  float: left;
+}
+
+.navbar {
+  float: right;
+  font-family: Helvetica;
+}
+
+.nav__item {
+  color: #ffffff;
+  text-decoration: none;
+  margin: 0 15px;
+  &:hover {
+    color: #fbce2a;
+  }
+}
 </style>
-
